b7416672887e45fe461f51bf32ec4f41e7b058be
Dev-JamesR <admin@ether1.org>
2020-02-24 20:26:40 +0000

    Misc Bug Fixes

--- upload.go
+++ upload.go
@@ -15,12 +15,12 @@ import (
 
 var controllerContractAddress = "0xc38B47169950D8A28bC77a6Fa7467464f25ADAFc"
 
-func CalculateUploadCost(contractDuration uint32, uploadSize uint32) uint32 {
+func CalculateUploadCost(contractDuration int32, uploadSize int32) int32 {
 	client, err := ethclient.Dial(rpcLocation)
 	if err != nil {
 		log.Fatal(err)
 	}
-	
+
 	address := common.HexToAddress(controllerContractAddress)
 	instance, err := NewEthoFSController(address, client)
 	if err != nil {
@@ -28,13 +28,34 @@ func CalculateUploadCost(contractDuration uint32, uploadSize uint32) uint32 {
 	}
 
 	// Get hosting cost
-	hostingCost, err := instance.GetNodeId(&bind.CallOpts{}, common.HexToAddress(nodeAddress))
+	hostingCost, err := instance.HostingCost(&bind.CallOpts{})
 	if err != nil {
 		log.Fatal(err)
 	}
-	cost := ((uploadSize / 1048576) * hostingCost) * (contractDuration / 46522)
-	
+	cost := ((uploadSize / 1048576) * int32(hostingCost.Int64())) * (contractDuration / 46522)
+
 	return cost
+}
+
+func CheckAccountExistence(accountAddress common.Address) bool {
+	client, err := ethclient.Dial(rpcLocation)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	address := common.HexToAddress(controllerContractAddress)
+	instance, err := NewEthoFSController(address, client)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check account existence
+	exists, err := instance.CheckAccountExistence(&bind.CallOpts{}, accountAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return exists
 }
 
 func UploadData(key string, contractCost uint64, mainHash string, contractName string, contractDuration uint32, uploadSize uint32, contentHashString string, contentPathString string) {
@@ -53,33 +74,39 @@ func UploadData(key string, contractCost uint64, mainHash string, contractName s
 	}
 
 	fromAddress := crypto.PubkeyToAddress(*publicKeyECDSA)
-	nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	gasPrice, err := client.SuggestGasPrice(context.Background())
-	if err != nil {
-		log.Fatal(err)
-	}
+	if CheckAccountExistence(fromAddress) {
+		nonce, err := client.PendingNonceAt(context.Background(), fromAddress)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	auth := bind.NewKeyedTransactor(privateKey)
-	auth.Nonce = big.NewInt(int64(nonce))
-	auth.Value = new(big.Int).Mul(big.NewInt(int64(contractCost)), big.NewInt(1e+18)) // in wei
-	auth.GasLimit = uint64(3000000) // in units
-	auth.GasPrice = gasPrice
+		gasPrice, err := client.SuggestGasPrice(context.Background())
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	address := common.HexToAddress(controllerContractAddress)
-	instance, err := NewEthoFSController(address, client)
-	if err != nil {
-		log.Fatal(err)
-	}
+		auth := bind.NewKeyedTransactor(privateKey)
+		auth.Nonce = big.NewInt(int64(nonce))
+		auth.Value = new(big.Int).Mul(big.NewInt(int64(contractCost)), big.NewInt(1e+18)) // in wei
+		auth.GasLimit = uint64(3000000) // in units
+		auth.GasPrice = gasPrice
 
-	// Initiaite upload tx
-	tx, err := instance.AddNewContract(auth, mainHash, contractName, contractDuration, uploadSize, uploadSize, contentHashString, contentPathString)
-	if err != nil {
-		log.Fatal(err)
+		address := common.HexToAddress(controllerContractAddress)
+		instance, err := NewEthoFSController(address, client)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Initiaite upload tx
+		tx, err := instance.AddNewContract(auth, mainHash, contractName, contractDuration, uploadSize, uploadSize, contentHashString, contentPathString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("Upload Tx Sent: %s", tx.Hash().Hex())
+		fmt.Println("\n")
+	} else {
+		fmt.Println("Unable to find valid hosting account, please register your address")
+		log.Fatal("\n")
 	}
-	fmt.Printf("Upload Tx Sent: %s", tx.Hash().Hex())
-	fmt.Println("\n")
 }
