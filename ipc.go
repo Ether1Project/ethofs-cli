b3b21237e5848650757d994d8daeae8d1b09cf4f
Dev-JamesR <admin@ether1.org>
2020-02-29 07:55:12 +0000

    Adding ethoFS IPC/RPC Services

---
+++ ipc.go
@@ -0,0 +1,26 @@
+import (
+        "errors"
+        "net"
+        "github.com/ethereum/go-ethereum/rpc"
+)
+
+type EthofsService struct {}
+
+func (s *EthofsService) Register(name, key string) string {
+        return "Registered " + name
+}
+
+func (s *EthofsService) Add(path string, key string, blocks uint64) (string, error) {
+        if blocks == 0 {
+                return "ethofs Data Upload Failed", errors.New("block count is too low")
+        }
+        return "ethoFS Data Upload Complete", nil
+}
+
+func ethofsIpc() {
+        ethofs := new(EthofsService)
+        server := rpc.NewServer()
+        server.RegisterName("ethofs", ethofs)
+        l, _ := net.ListenUnix("unix", &net.UnixAddr{Net: "unix", Name: "ethofs.ipc"})
+        server.ServeListener(l)
+}
